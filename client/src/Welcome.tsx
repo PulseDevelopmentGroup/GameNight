0bc1ce915f6afb768d93a33e54bb5e6f5394cf65
Josiah Nunemaker <josiah.nunemaker@gmail.com>
2020-06-07 20:51:38 -0400

    Begin adding landing page

---
+++ client/src/Welcome.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export const Welcome = () => {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (values: Record<string, any>) => {
+    console.log(values);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Room Code
+          <input name="roomCode" ref={register} />
+        </label>
+        <label>
+          Name
+          <input name="username" ref={register} />
+        </label>
+        <button type="submit">Join Room</button>
+      </form>
+    </div>
+  );
+};

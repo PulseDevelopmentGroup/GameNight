4489467aa55fd0d1a63bb460f4351705adbe8438
CS-5 <me@carsonseese.com>
2020-06-17 16:15:02 -0400

    Fix imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
 import { Welcome } from "./Welcome";
 
 function App() {

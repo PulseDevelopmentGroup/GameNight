2990d0577473fc80a93a6ba05c6b88ca34975e7f
Josiah Nunemaker <josiah.nunemaker@gmail.com>
2020-08-08 14:06:46 -0400

    Redirrect to menuy when no room code

--- client/src/room/RoomLobby.tsx
+++ client/src/room/RoomLobby.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useGetRoomCodeQuery } from "../generated/graphql";
+import { useHistory } from "react-router-dom";
 
 export const RoomLobby = () => {
+  const history = useHistory();
+  const { data } = useGetRoomCodeQuery();
+
+  const roomCode = data?.currentRoom;
+
+  useEffect(() => {
+    if (!roomCode) {
+      history.push("/");
+    }
+  }, [roomCode, history]);
+
   return <div>Welcome to the lobby :D</div>;
 };
